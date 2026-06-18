198eb4019dd01f89234c970b4be112288ff11321
Kelvin Ngari <ngarikelvin1015@gmail.com>
2026-06-18 09:41:14 +0300

    Add global CORS configuration

---
+++ src/main/java/com/gkev/InvoicingSystem/Config/CorsGlobalConfiguration.java
@@ -0,0 +1,30 @@
+package com.gkev.InvoicingSystem.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+    @Configuration
+    public class CorsGlobalConfiguration {
+
+        @Bean
+        public CorsWebFilter corsWebFilter() {
+            CorsConfiguration corsConfig = new CorsConfiguration();
+            corsConfig.setAllowedOrigins(List.of("http://localhost:5173"));
+            corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+            corsConfig.setAllowedHeaders(List.of("*"));
+            corsConfig.setAllowCredentials(true);
+
+            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+            source.registerCorsConfiguration("/**", corsConfig);
+
+            return new CorsWebFilter(source);
+        }
+    }
+
